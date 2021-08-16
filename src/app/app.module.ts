36f730debf8cd6503ae1bcbfcce9b4b0ef2f4a47
vuongvantu <vuongvantu2001x@gmail.com>
2021-08-16 16:01:01 +0700

    demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FontSizeEditorComponent
   ],
   imports: [
     BrowserModule,
